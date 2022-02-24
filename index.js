f46fad72d3686b0398622038488cfaa47afa83aa
Caleb Hamernick <caleb.hamernick@gmail.com>
2022-02-24 10:33:31 -0500

    commit tree 6138b18d0a9d941b2b9f4f16ea742c6c515161ae

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+const cats = [
+    "Milo", 
+    "Otis", 
+    "Garfield"];
+cats;
+
+function destructivelyAppendCat(name) {
+cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat(name){
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat(name){
+    cats.shift()
+}
+
+function appendCat(name){
+    const allCats = [...cats, "Broom"];
+    return allCats;
+}
+
+function prependCat(name){
+    const allCatsPrepend = ["Arnold", ...cats]
+    return allCatsPrepend
+}
+
+function removeLastCat(name){
+    cats.slice(0 ,-1);
+    return cats.slice(0, -1);
+}
+
+function removeFirstCat(name){
+    cats.slice(1);
+    return cats.slice(1);
+}
